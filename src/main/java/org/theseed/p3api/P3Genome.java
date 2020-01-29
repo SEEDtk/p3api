4998fc8dd62b1f5e1949290d741b5cbe9ccb2bd8
Bruce Parrello <bparrello@figresearch.com>
2020-01-29 06:55:19 -0600

    Fix for invalid tax IDs.

--- src/main/java/org/theseed/p3api/P3Genome.java
+++ src/main/java/org/theseed/p3api/P3Genome.java
@@ -74,12 +74,15 @@ public class P3Genome extends Genome {
             // Create the genome object.
             retVal = new P3Genome(genome_id);
             retVal.p3Store(genomeData);
-            // Get the genetic code.  It only works if we have a lineage.
+            // Get the genetic code.  It only works if we have a lineage.  We default to 11.
+            int code = 11;
             String[] lineages = retVal.getLineage();
             if (lineages.length > 0) {
                 JsonObject taxData = p3.getRecord(Table.TAXONOMY, lineages[lineages.length - 1], "genetic_code");
-                retVal.setGeneticCode(Connection.getInt(taxData, "genetic_code"));
+                // Some of the genomes have invalid taxonomy data, so we have to check here.
+                if (taxData != null) code = Connection.getInt(taxData, "genetic_code");
             }
+            retVal.setGeneticCode(code);
             // Process the contigs.  If the detail level is FULL, we get the DNA, too.
             String contigFields = (detail.includes(Details.FULL) ? "sequence_id,sequence" : "sequence_id,length");
             Collection<JsonObject> contigs = p3.query(Table.CONTIG, contigFields, Criterion.EQ("genome_id", genome_id));
