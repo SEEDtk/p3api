9fa8788420f4955ce9cf32c3586aac1453b14104
Bruce Parrello <bparrello@figresearch.com>
2021-11-09 10:12:34 -0600

    Fixed bugs in testing.

--- src/test/java/org/theseed/cli/TestRnaSource.java
+++ src/test/java/org/theseed/cli/TestRnaSource.java
@@ -21,8 +21,9 @@ public class TestRnaSource {
         JsonObject parms = new JsonObject();
         RnaSource source1 = new RnaSource.SRA("SRR123456");
         source1.store(parms);
-        JsonArray sourceParm = (JsonArray) parms.get("srr_ids");
-        assertThat(sourceParm.get(0), equalTo("SRR123456"));
+        JsonArray sourceParm = (JsonArray) parms.get("srr_libs");
+        JsonObject sourceStructure = (JsonObject) sourceParm.get(0);
+        assertThat(sourceStructure.get("srr_accession"), equalTo("SRR123456"));
         assertThat(sourceParm.size(), equalTo(1));
         RnaSource source2 = new RnaSource.Paired("file1", "file2");
         source2.store(parms);
